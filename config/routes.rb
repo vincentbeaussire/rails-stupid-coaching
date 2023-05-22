9947e444ebccee0d6a694df810112ea3258f1733
Vincent Beaussire <vincentpolska@gmail.com>
2023-05-22 15:06:05 +0200

    Stupid Coaching

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "ask", to: "questions#ask"
+  get "answer", to: "questions#coach_answer"
 
+  # avec le verbe get et l'url "wwww.monsite.com/answer" je vais dans le controller "questions" et l'action "coach_anwser"
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
 end
